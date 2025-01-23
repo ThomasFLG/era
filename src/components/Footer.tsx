c8c8507a001b857b72a3621a27173a28baaff984
ThomasFLG <159135867+ThomasFLG@users.noreply.github.com>
2025-01-23 11:18:07 +0100

    Delete src/components directory

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/components/Footer.tsx
-export default function Footer() {
-    return (
-      <footer>
-        <p>&copy; 2025 Mon Application. Tous droits réservés.</p>
-      </footer>
-    );
-  }
-  
