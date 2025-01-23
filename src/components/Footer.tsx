6ef7c8977b0b9871140ed5ac5d3bfae8c62c9a8f
ThomasFLG <159135867+ThomasFLG@users.noreply.github.com>
2025-01-23 11:16:30 +0100

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+// src/app/components/Footer.tsx
+export default function Footer() {
+    return (
+      <footer>
+        <p>&copy; 2025 Mon Application. Tous droits réservés.</p>
+      </footer>
+    );
+  }
+  
