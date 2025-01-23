753ddc51dda01f580521159111d3b970606bebaa
ThomasFLG <159135867+ThomasFLG@users.noreply.github.com>
2025-01-23 11:05:53 +0100

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import SurveyList from "../components/SurveyList";
-
-export default function Home() {
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100">
-            <SurveyList />
-        </div>
-    );
-}
