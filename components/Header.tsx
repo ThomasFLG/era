13d9ebf05b240eb94263fc2dfdc3a94d8247bf7b
ThomasFLG <159135867+ThomasFLG@users.noreply.github.com>
2025-01-23 11:06:45 +0100

    Delete components directory

--- components/Header.tsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-    return (
-      <header className="p-4 bg-blue-500 text-white text-center">
-        <h1>ERA</h1>
-      </header>
-    );
-  };
-  
-  export default Header;
